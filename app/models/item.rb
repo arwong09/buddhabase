056c2752d0908d6594e26c0ca6c3ed5ac2bad92b
Giff <giff.huang@futureadvisor.com>
2014-12-22 17:17:42 -0800

    changed sku generation callback

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ActiveRecord::Base
   attr_accessor :image_url
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  before_save :generate_new_sku
+  before_create :generate_new_sku
 
   def generate_new_sku
     chars = self.name.slice(0..2)
